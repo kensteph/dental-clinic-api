9d298a1af3b8d265257d4586ca5e9b865c8891bf
Kender <kenderromain@gmail.com>
2023-09-01 08:42:46 -0400

    Create my server using express.

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+// ========== MIDDLEWARES ==============
+// Middleware for parsing JSON in requests
+app.use(bodyParser.json());
+
+// ========== ROUTES ===============
+
+const PORT = process.env.PORT || 8788;
+
+// Listen to requests
+app.listen(PORT, () => console.log(`Welcome to Dental Clinic API. PORT ${PORT} `));
