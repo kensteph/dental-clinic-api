a547d8e461b4e42ecc99b27c27cb004d374be169
Kender <kenderromain@gmail.com>
2023-09-02 12:24:18 -0400

    Fix linter errors.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
 // ========== ROUTES ===============
-
-import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
@@ -11,24 +8,8 @@ const app = express();
 // Middleware for parsing JSON in requests
 app.use(bodyParser.json());
 
-// TEST
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn'],
-});
-
-// * CREATE
-const createUser = await prisma.person.create({
-  data: {
-    first_name: 'Kender',
-    last_name: 'Romain',
-    phone: '8626228731',
-    address: '95 CUMMINGS ST',
-  },
-});
-
-console.log(createUser);
-
 const PORT = process.env.PORT || 8788;
 
 // Listen to requests
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Welcome to Dental Clinic API. Listen to PORT : ${PORT} `));
