ca4f3fd8531493acd6917d258467900d9735b765
Kender <kenderromain@gmail.com>
2023-09-03 13:26:00 -0400

    Implement createUser function.

---
+++ controllers/v1/userController.js
@@ -0,0 +1,89 @@
+import { prisma } from '../../db/index.js';
+
+// Sample data (replace with your database interactions)
+const users = [
+  { id: 1, name: 'John Doe', email: 'john@example.com' },
+  { id: 10, name: 'Jane Smith', email: 'jane@example.com' },
+];
+
+// Get all users
+const getUsers = (req, res) => {
+  res.json({ users });
+};
+
+// Create a new user
+const createUser = async (req, res) => {
+  const password = 'werrrrwwq';
+  const { firstname, lastname, email, phone, address, username } = req.body;
+
+  const save = await prisma.user.create({
+    data: {
+      user_name: username,
+      email,
+      password,
+
+      person: {
+        create: {
+          first_name: firstname,
+          last_name: lastname,
+          phone,
+          address,
+        },
+      },
+    },
+  });
+  res.status(201).json({ message: 'User created', user: save });
+};
+
+// Get user by 
+const getUserById = (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const user = users.find((u) => u.id === userId);
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json({ user });
+};
+
+// Get user by ID
+const getUserById = (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const user = users.find((u) => u.id === userId);
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json({ user });
+};
+
+// Update user by ID
+const updateUser = (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const { name, email } = req.body;
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  user.name = name || user.name;
+  user.email = email || user.email;
+
+  res.json({ message: 'User updated', user });
+};
+
+// Delete user by ID
+const deleteUser = (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const userIndex = users.findIndex((u) => u.id === userId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  users.splice(userIndex, 1);
+  res.json({ message: 'User deleted' });
+};
+
+export { getUsers, getUserById, createUser, updateUser, deleteUser };
---
+++ routes/v1/userRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+  getUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from '../../controllers/v1/userController.js';
+
+const router = express.Router();
+
+// Define routes for version 1 of the API
+router.get('/api/v1/users', getUsers);
+router.post('/api/v1/users', createUser);
+router.get('/api/v1/users/:id', getUserById);
+router.put('/api/v1/users/:id', updateUser);
+router.delete('/api/v1/users/:id', deleteUser);
+
+export default router;
